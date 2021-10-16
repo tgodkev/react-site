741460585eb581ba9fc8eddb4f92b931518ee3d6
kevin knight <tgodkev@gmail.com>
2021-10-16 16:50:31 -0400

    first save point

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import  {Link}  from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        TRVL <i className="fab fa-typo3"></i>
+                    </Link>
+                </div>
+
+            </nav>
+        
+        </div>
+    )
+}
+
+export default Navbar;
