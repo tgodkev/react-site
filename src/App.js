b412c39663900ccf9f428eb07b8b1a4ae425dc71
kevin knight <tgodkev@gmail.com>
2021-10-17 16:53:44 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from './components/Pages/Home'
+import Home from './components/Pages/Home';
 
 function App() {
   return (
